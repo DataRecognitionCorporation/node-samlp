49fddd21703760225bdde62bfcea7a2f8c592dc3
Barry Svee <72763059+drc-bsvee@users.noreply.github.com>
2026-04-03 17:39:29 -0500

    B1-2264: restore omitted change from _v7 branch

--- test/metadata.tests.js
+++ test/metadata.tests.js
@@ -80,9 +80,14 @@ describe('samlp metadata', function () {
 
   });
 
+  // DRC: x-forwarded-host is intentionally NOT applied to the host portion of
+  // metadata URLs. Use options.absoluteUrls=true with absolute endpointPath
+  // values when deploying behind a reverse proxy that changes the hostname.
+  // x-forwarded-proto IS respected for the protocol portion.
   describe('request to metadata with proxy', function () {
-    var doc;
+    var docHost, docProto;
     before(function (done) {
+      // Test that x-forwarded-host does NOT change the host in URLs
       request.get({
         jar: request.jar(),
         uri: 'http://localhost:5050/samlp/FederationMetadata/2007-06/FederationMetadata.xml',
@@ -91,24 +96,41 @@ describe('samlp metadata', function () {
         }
       }, function (err, response, b) {
         if (err) return done(err);
-        doc = new xmldom.DOMParser().parseFromString(b).documentElement;
-        done();
+        docHost = new xmldom.DOMParser().parseFromString(b).documentElement;
+
+        // Test that x-forwarded-proto DOES change the protocol in URLs
+        request.get({
+          jar: request.jar(),
+          uri: 'http://localhost:5050/samlp/FederationMetadata/2007-06/FederationMetadata.xml',
+          headers: {
+            'X-Forwarded-Proto': 'https'
+          }
+        }, function (err2, response2, b2) {
+          if (err2) return done(err2);
+          docProto = new xmldom.DOMParser().parseFromString(b2).documentElement;
+          done();
+        });
       });
     });
 
-    it('should have the redirect endpoint url with the forwarded host', function () {
-      expect(doc.getElementsByTagName('SingleSignOnService')[0].getAttribute('Location'))
-        .to.equal('http://myserver.com/samlp/123');
+    it('should ignore x-forwarded-host and use the actual request host', function () {
+      expect(docHost.getElementsByTagName('SingleSignOnService')[0].getAttribute('Location'))
+        .to.equal('http://localhost:5050/samlp/123');
     });
 
-    it('should have the POST endpoint url with the forwarded host', function () {
-      expect(doc.getElementsByTagName('SingleSignOnService')[1].getAttribute('Location'))
-        .to.equal('http://myserver.com/login/callback');
+    it('should ignore x-forwarded-host for the POST endpoint url', function () {
+      expect(docHost.getElementsByTagName('SingleSignOnService')[1].getAttribute('Location'))
+        .to.equal('http://localhost:5050/login/callback');
     });
 
-    it('should have the logout endpoint url with the forwarded host', function () {
-      expect(doc.getElementsByTagName('SingleLogoutService')[0].getAttribute('Location'))
-        .to.equal('http://myserver.com/logout');
+    it('should ignore x-forwarded-host for the logout endpoint url', function () {
+      expect(docHost.getElementsByTagName('SingleLogoutService')[0].getAttribute('Location'))
+        .to.equal('http://localhost:5050/logout');
+    });
+
+    it('should use x-forwarded-proto for the protocol in endpoint urls', function () {
+      expect(docProto.getElementsByTagName('SingleSignOnService')[0].getAttribute('Location'))
+        .to.equal('https://localhost:5050/samlp/123');
     });
 
   });
